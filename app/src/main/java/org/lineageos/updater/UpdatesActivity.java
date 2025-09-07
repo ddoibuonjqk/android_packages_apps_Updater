3808404c698f441ef04a5658ea8d2605d26c6bc7
nhansp <nhan.094.vn@gmail.com>
2025-09-07 15:19:15 +0700

    Updater: Replace header title with header image

--- app/src/main/java/org/lineageos/updater/UpdatesActivity.java
+++ app/src/main/java/org/lineageos/updater/UpdatesActivity.java
@@ -183,10 +183,6 @@ public class UpdatesActivity extends UpdatesListActivity implements UpdateImport
             }
         }
 
-        TextView headerTitle = findViewById(R.id.header_title);
-        headerTitle.setText(getString(R.string.header_title_text,
-                BuildInfoUtils.getBuildVersion()));
-
         updateLastCheckedString();
 
         TextView headerBuildVersion = findViewById(R.id.header_build_version);
